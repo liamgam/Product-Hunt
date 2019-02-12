954033fafc1e7c4a6481d68302f535d8402af7b4
liamgam <liam.liamgamble@gmail.com>
2019-02-11 21:38:25 -0800

    created post model

---
+++ ProductHunt/Post.swift
@@ -0,0 +1,19 @@
+//
+//  Post.swift
+//  ProductHunt
+//
+//  Created by Rinni Swift on 2/11/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
+
+
