8701da8a83fadb40a86f16107673816199371c06
liamgam <liam.liamgamble@gmail.com>
2019-02-11 21:35:07 -0800

    tableview setup

---
+++ ProductHunt/FeedViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FeedViewController.swift
+//  ProductHunt
+//
+//  Created by Rinni Swift on 2/11/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+
+}
+
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+}
+
+extension FeedViewController: UITableViewDelegate {
+    
+}
