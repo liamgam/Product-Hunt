770f0cbfcf3123030e53dbf528f408fb103fbfd2
liamgam <liam.liamgamble@gmail.com>
2019-03-19 19:42:05 -0700

    fix up

--- ShopifyInternApp/ShopifyInternApp/Controller/CollectionDetailsViewController.swift
+++ ShopifyInternApp/ShopifyInternApp/Controller/CollectionDetailsViewController.swift
@@ -35,6 +35,7 @@ class CollectionDetailsViewController: UIViewController, UICollectionViewDelegat
             self.collectionView.dataSource = self
         }
         collectionTitle.text = collectionTitleString
+        flowLayout()
     }
     
     func flowLayout() {
